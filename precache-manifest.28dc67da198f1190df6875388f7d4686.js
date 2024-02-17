735c23fbe87880dff6081f0d666290aefe6aaf8d
shapkarin <yu.shapkarin@gmail.com>
2024-02-17 04:10:32 +0000

    Deploying to gh-pages from @ shapkarin/metrics-crypto@91458e0a442aba664d8504c408e9c81387ad4794 🚀

---
+++ precache-manifest.28dc67da198f1190df6875388f7d4686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4ce1986b20a029c746f1121f2a6c72",
+    "url": "/metrics-crypto/index.html"
+  },
+  {
+    "revision": "eb660a318b166e47191a",
+    "url": "/metrics-crypto/static/css/main.384b05cf.chunk.css"
+  },
+  {
+    "revision": "7dd94faa5ead82295338",
+    "url": "/metrics-crypto/static/js/2.17fbc665.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/metrics-crypto/static/js/2.17fbc665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb660a318b166e47191a",
+    "url": "/metrics-crypto/static/js/main.a698a719.chunk.js"
+  },
+  {
+    "revision": "3d6064291e8709476803",
+    "url": "/metrics-crypto/static/js/runtime-main.d23e7631.js"
+  }
+]);
